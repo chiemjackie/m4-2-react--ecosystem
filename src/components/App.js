8490ed093988c2022e573cdfe95093b1b9d6b45d
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-07-25 00:58:11 -0400

    not sure what just happened, but I undid it

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
+
 import GlobalStyles from './GlobalStyles';
 
 import Header from './Header';
@@ -9,20 +9,7 @@ import About from './About';
 import ItemDetails from './ItemDetails'
 
 const App = ({ items }) => {
-=======
-import styled from "styled-components";
-import Header from './Header.js'
 
-const Homepage = () => {
-  return <div>Homepage</div>
-}
-
-const About = () => {
-  return <div>About</div>
-}
-
-const App = () => {
->>>>>>> e08963abd6afa2a48cfbd3c5ed9438158da1d634
   return (
     <div>
       <GlobalStyles />
